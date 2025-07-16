aaf659e89af0fae0a5928c3cb1584ec329d02c9a
maxim <terror20007@mail.ru>
2025-07-16 17:58:46 +0300

    add: redis

--- src/api/v1/base/service.py
+++ src/api/v1/base/service.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass
 
 from crypto_processing.client import CryptoProcessingClient
 from infra.postgres.uow import PostgresUnitOfWork
+from infra.redis.redis_api import RedisAPI
 
 
 @dataclass(slots=True)
 class BaseService:
     uow: PostgresUnitOfWork
     crypto_processing_client: CryptoProcessingClient | None = None
+    redis: RedisAPI | None = None
---
+++ src/infra/redis/dependencies.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator, Annotated
+from fastapi import Depends
+
+from infra.redis.redis_api import RedisAPI
+
+
+@asynccontextmanager
+async def get_redis() -> AsyncIterator[RedisAPI]:
+    redis_client = RedisAPI()
+    try:
+        yield redis_client
+    finally:
+        await redis_client.close()
+
+RedisDep = Annotated[RedisAPI, Depends(get_redis)]
