63ed1fcf1d0fcca833926185661c2156f9e10d4b
maxim <terror20007@mail.ru>
2025-07-16 19:10:06 +0300

    fix: alfa config

--- src/core/config/components/alfa.py
+++ src/core/config/components/alfa.py
@@ -5,12 +5,11 @@ from core.config.constants import ENV_FILE_PATH
 
 
 class AlfaApiConfig(BaseSettings):
-    base_url: str = Field("https://sandbox.alfabank.ru", description="Базовый URL сервиса СБП")
-    client_id: str = Field("0cee0683-85ae-49f2-a63d-29f97aad1911", description="Идентификатор партнёрского сервиса")
-    client_secret: str = Field(default="Qwerty1234567890Qwerty1234567890!", description="Секретный ключ партнёрского сервиса")
+    alfa_base_url: str = Field(default="https://sandbox.alfabank.ru", description="Базовый URL сервиса СБП")
+    alfa_client_id: str = Field(default="0cee0683-85ae-49f2-a63d-29f97aad1911", description="Идентификатор партнёрского сервиса")
+    alfa_client_secret: str = Field(default="Qwerty1234567890Qwerty1234567890!", description="Секретный ключ партнёрского сервиса")
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE_PATH,
         env_file_encoding='utf-8',
-        env_prefix="ALFA_",
     )
