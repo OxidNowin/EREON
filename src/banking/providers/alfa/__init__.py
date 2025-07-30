19e1c3d2d279e90dd509870446e8b801c4ee43d9
maxim <terror20007@mail.ru>
2025-07-30 21:59:09 +0300

    fix: imports

--- src/banking/providers/alfa/__init__.py
+++ src/banking/providers/alfa/__init__.py
@@ -1,4 +1,6 @@
 from banking.providers.alfa.client import AlfaClient
+from banking.providers.alfa.client import AlfaScope
+from banking.providers.alfa.token_service import AlfaTokenService
 from banking.providers.alfa.schemas import (
     PaymentLinkData, 
     PaymentStatusResponse,
@@ -14,6 +16,8 @@ from banking.providers.alfa.exceptions import AlfaApiError, AlfaTokenError
 
 __all__ = [
     "AlfaClient",
+    "AlfaTokenService",
+    "AlfaScope",
     "PaymentLinkData",
     "PaymentStatus",
     "QRCodeType",
