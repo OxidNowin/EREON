34d58c00fae9a1548ee5d154fe20863b41257c89
maxim <terror20007@mail.ru>
2025-08-04 18:06:27 +0300

    change: table sbp_payment

--- src/infra/postgres/models/__init__.py
+++ src/infra/postgres/models/__init__.py
@@ -3,7 +3,7 @@ from infra.postgres.models.referral import Referral
 from infra.postgres.models.wallet import Wallet, WalletStatus, WalletCurrency
 from infra.postgres.models.operation import Operation, OperationStatus, OperationType
 from infra.postgres.models.cryptocurrency_replenishment import CryptocurrencyReplenishment
-from infra.postgres.models.sbp_payment import SbpPayment
+from infra.postgres.models.sbp_payment import SbpPayment, SbpPaymentStatus
 
 
 __all__ = [
@@ -17,4 +17,5 @@ __all__ = [
     "OperationStatus",
     "CryptocurrencyReplenishment",
     "SbpPayment",
+    "SbpPaymentStatus",
 ]
--- src/infra/postgres/models/sbp_payment.py
+++ src/infra/postgres/models/sbp_payment.py
@@ -1,12 +1,20 @@
 from uuid import UUID
 from decimal import Decimal
+from enum import Enum as PyEnum
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, DECIMAL, text, Integer, String, Text
+from sqlalchemy import ForeignKey, DECIMAL, text, Integer, Text
 from sqlalchemy.dialects.postgresql import UUID as PGUUID
+from sqlalchemy.sql.sqltypes import Enum
 
 from infra.postgres.models.base import Base
 from infra.postgres.mixins import CreateTimestampMixin
+
+
+class SbpPaymentStatus(PyEnum):
+    CONFIRMED = "confirmed"
+    PENDING = "pending"
+    CANCELLED = "cancelled"
 
 
 class SbpPayment(Base, CreateTimestampMixin):
@@ -30,11 +38,13 @@ class SbpPayment(Base, CreateTimestampMixin):
     crypto_amount: Mapped[Decimal] = mapped_column(DECIMAL(precision=20, scale=6), nullable=False)
     fee_crypto: Mapped[Decimal] = mapped_column(DECIMAL(precision=20, scale=6), nullable=False, server_default="0")
     total_amount_crypto: Mapped[Decimal] = mapped_column(DECIMAL(precision=20, scale=6), nullable=False)
-    currency: Mapped[str] = mapped_column(String(32), nullable=False)
     exchange: Mapped[Decimal] = mapped_column(DECIMAL(precision=20, scale=6), nullable=False)
     sbp_url: Mapped[str] = mapped_column(Text, nullable=False)
     outgoing_payment_id: Mapped[str] = mapped_column(Text, nullable=False)
-    status: Mapped[str] = mapped_column(String(32), nullable=False)
+    status: Mapped[SbpPaymentStatus] = mapped_column(
+        Enum(SbpPaymentStatus, name="sbp_payment_status_enum", native_enum=False),
+        nullable=False,
+    )
 
     operation: Mapped["Operation"] = relationship("Operation", back_populates="sbp_payments") 
     
