373e409eaa30763bcc5a0d5a7e5ecc842365bc83
maxim <terror20007@mail.ru>
2025-09-22 19:07:50 +0300

    add: icon to wallet response

--- src/api/v1/wallet/schemas.py
+++ src/api/v1/wallet/schemas.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict, field_validator, Field
+from pydantic import BaseModel, ConfigDict, field_validator, Field, model_validator
 
 from infra.postgres.models import WalletCurrency
 from crypto_processing.network import matcher
@@ -17,8 +17,18 @@ class WalletResponse(BaseModel):
     currency: WalletCurrency = Field(..., description="Тип криптовалюты кошелька", examples=["USDT"])
     balance: Decimal = Field(..., description="Текущий баланс кошелька в основной валюте")
     addresses: list[Address] = Field(..., description="Список адресов для пополнения кошелька")
+    icon: str = Field("")
 
     model_config = ConfigDict(from_attributes=True)
+
+    @model_validator(mode="after")
+    def set_icon_by_currency(self) -> "WalletResponse":
+        currency_to_icon: dict[WalletCurrency, str] = {
+            WalletCurrency.USDT: "https://assets.coingecko.com/coins/images/325/large/Tether-logo.png",
+        }
+        if not self.icon:
+            self.icon = currency_to_icon.get(self.currency, "")
+        return self
 
     @field_validator("addresses", mode="before")
     def parse_addresses(cls, raw_addresses: list[str]) -> list[dict]:
